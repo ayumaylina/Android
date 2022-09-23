cf506fb9d08d6c0b65d7e70727d229274f16600e
Ayu <maylinayu8@gmail.com>
2022-09-23 22:59:31 +0700

    commit appium

---
+++ Android/src/main/java/coid/nexsoft/app/runner/CalculatorDestroyer.java
@@ -0,0 +1,134 @@
+package coid.nexsoft.app.runner;
+
+import static org.testng.Assert.assertEquals;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Random;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.json.simple.JSONObject;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import coid.nexsoft.app.pageobject.CalcDestroyer;
+import coid.nexsoft.app.utils.ExcelReader;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class CalculatorDestroyer {
+
+	private static AndroidDriver<MobileElement> driver;
+	private CalcDestroyer calcDestroyer;
+	private ExcelReader excelReader;
+	private Object [][] dDriven ;
+	private int intColumnNums;
+	private int intRowNums;
+	private int intNumX=0;
+	private int intNumNext = 0;
+	private int intLoopCalc =0;
+	private int intOperator = 0;
+	private double doubResultExpected = 0.0;
+	private double doubResultActual = 0.0;
+	private Random rand;
+	
+	//private int
+	
+	@BeforeTest
+	public void befTest()
+	{
+		
+		try {
+			
+			rand = new Random();
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setCapability("deviceName", "G011A");
+			capabilities.setCapability("uuid", "127.0.0.1:21513");
+			capabilities.setCapability("platformName", "Android");
+			capabilities.setCapability("platformVersion", "7.1.2");
+			capabilities.setCapability("appPackage", "com.google.android.calculator");
+			capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+			
+			driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+			
+			String excelPath = "./data/DataDriven.xlsx";
+			String sheetName = "Sheet1";
+			excelReader = new ExcelReader(excelPath, sheetName);
+			intRowNums = excelReader.getRowCount();
+			intColumnNums = excelReader.getColCount();
+			calcDestroyer = new CalcDestroyer(driver);
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@DataProvider(name = "DataProviderFirst")
+	public Object[][] dataDrivenPost()
+	{
+		dDriven = new Object[intRowNums][intColumnNums];
+		
+		Iterator<Row> rX = excelReader.getIter();
+		int a=0;
+		
+		while(rX.hasNext())
+		{
+			Row rows = rX.next();
+			for(int j=0;j<intColumnNums;j++)
+			{
+				dDriven[a][j] = excelReader.getCellData(a, j);
+				System.out.println(dDriven[a][j]);
+				//System.out.println("intNumX adalah "+intNumX);
+				//System.out.println("intOperator adalah "+intOperator);
+			}
+			a++;
+			//System.out.println("intOperator adalah "+intOperator);
+		}
+		return dDriven;		
+	}
+	
+	@Test(priority = 0,dataProvider="DataProviderFirst")
+	public void testPost(String x)
+	{		
+		System.out.println("");
+		System.out.println("================== looping ke "+x+" ==================");
+		intLoopCalc = rand.nextInt(15);
+		intNumX = rand.nextInt(10);
+		System.out.println("Angka pertama--> "+intNumX);
+		for(int i=0;i<intLoopCalc;i++)
+		{
+			
+			intNumNext = rand.nextInt(10);
+			System.out.println("Angka--> "+intNumNext);
+			intOperator = rand.nextInt(5);
+			System.out.println("Operator--> "+intOperator);
+			System.out.println("Note : Operator 0 --> pengurangan ");
+			System.out.println("       Operator 1 --> perkalian ");
+			System.out.println("       Operator 2 --> pembagian ");
+			System.out.println("       Operator 3 --> penjumlahan ");
+			System.out.println("       Operator 4 --> pengurangan ");
+//			public double getResult(double result,int number, int operator)
+			
+			if(i==0)
+			{
+				doubResultExpected = calcDestroyer.initCalc(intNumX,intNumNext, intOperator);
+				System.out.println("Hasil perhitungan "+doubResultExpected);
+			}
+			else
+			{
+				doubResultExpected = calcDestroyer.getResult(doubResultExpected,intNumNext,intOperator);
+				System.out.println("Hasil perhitungan "+doubResultExpected);
+			}
+		}
+		calcDestroyer.doEqualz();
+		//System.out.println("Hasil Calculator destroyer"+calcDestroyer); //calc destroyer
+		doubResultActual = calcDestroyer.getTxtResult();
+		//System.out.println("");
+		System.out.println("ACTUAL : "+doubResultActual+" --- EXPECTED : "+doubResultExpected);
+		assertEquals(doubResultActual, doubResultExpected);
+		calcDestroyer.clear();
+	}
+}
